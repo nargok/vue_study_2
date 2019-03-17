719985f96ade4b5bffc296cad91ac3249f332b41
nargok <nargok@gmail.com>
2019-03-17 22:07:42 +0900

    slot

---
+++ template/src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <h1>{{ title }}</h1>
+    <p>{{message}}</p>
+    <hr>
+    <div><slot /></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    title: String
+  },
+  data: function () {
+    return {
+      message: 'This is a message',
+    };
+  },
+}
+</script>
+
+<style>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.inner {
+  color: red;
+  font-size: 14pt;
+}
+</style>
