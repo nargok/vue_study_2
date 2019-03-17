2d3aea98ff5088432bcfbad071c46526cb321305
nargok <nargok@gmail.com>
2019-03-18 00:33:33 +0900

    Render

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,22 +1,25 @@
 <template>
   <div id="app">
-    <HelloWorld title="slot">
-      <p class="etc">Begin!</p>
-      <p class="inner" slot="a">***First message***</p>
-      <p class="etc">Middle...</p>
-      <p class="inner" slot="b">***Second message***</p>
-      <p class="etc">End.</p>
-      <p class="inner" slot="c">***Third message***</p>
-    </HelloWorld>
+    <!--<HelloWorld title="slot">-->
+      <!--<p class="etc">Begin!</p>-->
+      <!--<p class="inner" slot="a">***First message***</p>-->
+      <!--<p class="etc">Middle...</p>-->
+      <!--<p class="inner" slot="b">***Second message***</p>-->
+      <!--<p class="etc">End.</p>-->
+      <!--<p class="inner" slot="c">***Third message***</p>-->
+    <!--</HelloWorld>-->
+    <Render />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Render from "./components/Render";
 
 export default {
   name: 'app',
   components: {
+    Render,
     HelloWorld
   }
 }
---
+++ template/src/components/Render.vue
@@ -0,0 +1,11 @@
+<script>
+  export default {
+    name: "Render",
+    render: function (h) {
+      return h('div', {}, [
+        h('h1', 'Render'),
+        h('p', 'this is render sample.'),
+      ]);
+    }
+  }
+</script>
