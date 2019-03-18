cf8c49b8a2648d5945f2b2b7614c97dfbb4ef8bc
nargok <nargok@gmail.com>
2019-03-18 23:47:48 +0900

    [transition]flagで表示非表示をコントロールする

---
+++ transition/src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <h1>{{ title }}</h1>
+    <p>{{message}}</p>
+    <hr>
+    <button v-on:click="doAction">
+      {{btn}}
+    </button>
+    <transition name="transit">
+      <p v-if="flg" class="trans">Transition!</p>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    title: String
+  },
+  data: function () {
+    return {
+      message: 'Transition Sample!',
+      flg: true,
+      btn: 'Show/Hide',
+    };
+  },
+  methods: {
+    doAction: function () {
+      this.flg = !this.flg;
+    },
+  },
+}
+</script>
+
+<style>
+.trans {
+  background-color: #ddf;
+  padding: 10px;
+  font-size: 20pt;
+}
+</style>
